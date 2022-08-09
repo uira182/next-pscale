d9be5e49465cd1ce3c78e51663ad1b59f11145f8
Uirá de Souza <uira182@hotmail.com>
2022-08-09 12:53:39 -0300

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { GetStaticProps } from "next"
+import { useEffect } from "react"
+import { getUsers } from "../lib/users"
+
+export default function Home() {
+  function fetchUsers(){
+    fetch('api/users')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+  return (
+    <button onClick={fetchUsers}>Buscar</button>
+  )
+}
+
+
+export const getStaticProps: GetStaticProps = async () =>{
+  const users = await getUsers()
+
+  console.log(users)
+
+  return{
+    props: {
+      users,
+    },
+    revalidate: 5
+  }
+}
